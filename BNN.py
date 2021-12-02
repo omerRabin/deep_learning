f0e215439cb444b34fec4d94cad7427922562f1f
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-12-02 20:15:05 +0200

    Update BNN.py

--- BNN.py
+++ BNN.py
@@ -10,37 +10,86 @@ from nltk.stem import PorterStemmer
 ps = PorterStemmer() 
 
 vocabulary_size = 0
-word2location = {} # Bag of words
-data=data.sample(frac=1)
+word2location = {}
 
-# Construct the Bag of words
 def prepare_vocabulary(data):
     index = 0
-    counter = 0
     for sentance in data['text']:
-        counter +=1
-        if counter<4804*0.7:
-            words = nltk.word_tokenize(sentance) 
-            for word in words:
-                stemed_word = ps.stem(word) # make all letters lower
-
-                if stemed_word not in word2location:
-                    
-                    word2location[stemed_word] = index
-                    index += 1
-                    
-    return index # the size of our bag of words
+        #sentance = sentance.lower()
+        words = nltk.word_tokenize(sentance)
+        for word in words:
+            stemed_word = ps.stem(word)
+            if stemed_word not in word2location:
+                word2location[stemed_word] = index
+                index += 1
+    return index
 
 def convert2vec(sentance):
     #sentance = sentance.lower()
-    res_vec = np.zeros(vocabulary_size) # initialize the result by fill with zeros
-    words = nltk.word_tokenize(sentance) # split the verse into words
+    res_vec = np.zeros(vocabulary_size)
+    words = nltk.word_tokenize(sentance)
     for word in words:
-        stemed_word = ps.stem(word) # make the word in lower case
-        if stemed_word in word2location: 
-            res_vec[word2location[stemed_word]]+=1 # calculate the relative amount of the
-            # number of occurrences of the word out of all its occurrences in our entire data set
+        stemed_word = ps.stem(word)
+        if stemed_word in word2location:
+            res_vec[word2location[stemed_word]]+=1
     return res_vec
 
-books = ['Genesis', '1 Samuel','Psalms'] # our data set
+books = ['Genesis', '1 Samuel', 'Psalms']
 
+def encode(line):
+    res_vec = np.zeros(3)
+    idx = books.index(data.iloc[line]['book'])
+    res_vec[idx] = 1
+    return res_vec
+
+vocabulary_size = prepare_vocabulary(data)
+print("the size of the vocabulary is: ", vocabulary_size)
+import random
+tf.compat.v1.disable_v2_behavior()
+
+data_x = np.array([convert2vec(data.iloc[i]['text']) for i in range(len(data['text']))])
+data_y = np.array([encode(i) for i in range(len(data['text']))])
+
+features = vocabulary_size
+categories = 3 #number of books
+epsilon = 1e-12
+(hidden1_size) = (50)
+x = tf.compat.v1.placeholder(tf.float32,[None,features])
+y_ = tf.compat.v1.placeholder(tf.float32,[None,categories])
+
+W1 = tf.compat.v1.Variable(tf.compat.v1.truncated_normal([features, hidden1_size], stddev = 0.1))
+b1 = tf.compat.v1.Variable(tf.constant(0.1, shape = [hidden1_size]))
+z1 = tf.nn.relu(tf.matmul(x,W1)+b1)
+
+W2 = tf.compat.v1.Variable(tf.compat.v1.truncated_normal([hidden1_size, categories], stddev = 0.1))
+b2 = tf.compat.v1.Variable(tf.constant(0.1, shape = [categories]))
+z2 = tf.nn.relu(tf.matmul(z1,W2)+b2)
+
+y = tf.nn.softmax(tf.matmul(z1,W2)+b2)
+
+cross_entropy = tf.reduce_mean(-tf.compat.v1.reduce_sum(y_*tf.compat.v1.log(y), reduction_indices = [1]))
+train_step = tf.compat.v1.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
+
+sess = tf.compat.v1.Session()
+sess.run(tf.compat.v1.global_variables_initializer())
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    
+iteration_axis = np.array([])
+accuracy_axis = np.array([])
+rand = []
+
+for i in range (500):
+    for _ in range(1000):
+        for r in range (200):
+            ra = random.randrange(0, 4804)
+            rand.append(ra)
+        batch_xs = np.array([convert2vec(data.iloc[j]['text']) for j in rand])
+        batch_ys = np.array([encode(j) for j in rand])
+        rand = []
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    iteration_axis = np.append(iteration_axis, [i], axis=0)
+    accuracy_axis = np.append(accuracy_axis, [sess.run(accuracy, feed_dict={x: data_x, y_: data_y})], axis=0)
+    print("Iteration: ",i, " Accuracy:", sess.run(accuracy, feed_dict={x: data_x, y_: data_y}))
+    
